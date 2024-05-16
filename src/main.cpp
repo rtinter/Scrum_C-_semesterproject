18e6155f54bc6545c050bc88fde722a8529c010a
“gerrit” <koch.jangerrit@gmail.com>
2024-05-16 19:31:06 +0200

    test

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,26 @@
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
+#include <random>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
+    sf::RenderWindow window(sf::VideoMode(1920, 1280), "ImGui + SFML = <3");
     window.setFramerateLimit(60);
     ImGui::SFML::Init(window);
 
+/*
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
-
+*/
+    sf:
     sf::Clock deltaClock;
+    sf::Clock reactionTimer; // Timer for the reaction game
+    std::mt19937 rng(std::random_device{}()); // Random number generator
+    std::uniform_int_distribution<std::mt19937::result_type> dist6(0,8000); // Distribution range between 0 and 8000 milliseconds
+
+    float randomTime = dist6(rng); // Time to wait before changing to green
+    bool switchedToGreen = false; // State to track if the color has switched to green
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -27,14 +37,11 @@ int main() {
 
         ImGui::SFML::Update(window, deltaClock.restart());
 
-        ImGui::ShowDemoWindow();
+        //ImGui::ShowDemoWindow();
 
-        ImGui::Begin("Hello, world!");
-        ImGui::Button("Look at this pretty button");
-        ImGui::End();
 
         window.clear();
-        window.draw(shape);
+        /*window.draw(shape);*/
         ImGui::SFML::Render(window);
         window.display();
     }
