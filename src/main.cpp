e646f390c9d9463f00a4873500eee27abe797744
Sebastian Elisa Reineke <elisa.reineke@student.fh-kiel.de>
2024-05-16 19:24:11 +0200

    bug:  added ImGui::SFML::Shutdown();

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,27 @@
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
+//#include StyleManager.hpp
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Human Benchmark");
     window.setFramerateLimit(60);
 
     ImGui::SFML::Init(window);
+
+    //StyleManager::loadFonts();
+    //ImGuiIO &io{ImGui::GetIO()};
+
+    //ImFont *bigFont{io.Fonts->AddFontDefault()};
+    //bigFont->Scale = 3.f;
+
+    //ImFont *veryBigFont{io.Fonts->AddFontDefault()};
+    //veryBigFont->Scale = 6.f;
+
+    // Load font from other source
+    float fontSize = 18.0f; // Set font size
+    //io.Fonts->AddFontFromFileTTF("Roboto-Medium.ttf", fontSize); // can be replaced with other font
+
 
     sf::Clock deltaClock;
     while (window.isOpen()) {
@@ -27,8 +42,17 @@ int main() {
 
         ImGui::ShowDemoWindow();
 
+        /*
+        ImGui::Begin("test");
+        ImGuiIO &io{ImGui::GetIO()};
+        ImGui::PushFont(io.Fonts->Fonts[0]);
+        ImGui::Text("test");
+        ImGui::End();
+         */
+
         window.clear();
         ImGui::SFML::Render(window);
         window.display();
     }
+    ImGui::SFML::Shutdown();
 }
