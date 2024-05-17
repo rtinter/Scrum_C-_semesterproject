64a9384affcd59f8324ad683361cf5c4103ea3ce
Karlina Jung <karlina.jung@student.fh-kiel.de>
2024-05-17 14:59:53 +0200

    add: Platzhalter Text, runde Ecken

--- src/main.cpp
+++ src/main.cpp
@@ -21,15 +21,15 @@ int main() {
     bool showModal {false};
 
 
-        while (window.isOpen()) {
-            sf::Event event;
-            while (window.pollEvent(event)) {
-                ImGui::SFML::ProcessEvent(window, event);
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(window, event);
 
-                if (event.type == sf::Event::Closed) {
-                    window.close();
-                }
+            if (event.type == sf::Event::Closed) {
+                window.close();
             }
+        }
 
         ImGui::SFML::Update(window, deltaClock.restart());
 
@@ -38,6 +38,13 @@ int main() {
         //ImGui::ShowDemoWindow();
         //ImGui::ShowUserGuide();
 
+        ImGuiStyle &imGuiStyle{ImGui::GetStyle()};
+        imGuiStyle.FrameRounding = 5.0f;
+        imGuiStyle.WindowRounding = 5.0f;
+
+        ImGuiIO &io = ImGui::GetIO();
+        ImFont *font1 = io.Fonts->AddFontDefault();
+        ImFont *font2 = io.Fonts->AddFontFromFileTTF("path/to/another_font.ttf", 24.0f);
 
         ImGui::Begin("Test Window");
 
@@ -52,15 +59,33 @@ int main() {
         if (showModal) {
             ImGui::SetNextWindowSize(ImVec2(1000, 600));
 
-            ImGui::OpenPopup("Overlay??");
+            ImGui::OpenPopup("Overlay");
             showModal = false; //Zum resetten
         }
 
-        if (ImGui::BeginPopupModal("Overlay??", NULL, ImGuiWindowFlags_NoResize)) {
+        if (ImGui::BeginPopupModal("Overlay", NULL,
+                                   ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoTitleBar)) {
 
-            ImGui::Text("SPIELENAME");
-            ImGui::Text("SPIELBESCHREIBUNG");
-            ImGui::Text("SPIELREGELN");
+            ImGui::Text("TITEL: Reaktionsspiel");
+            ImGui::Text(
+                    "SPIELBESCHREIBUNG: Testen und verbessern Sie Ihre Reaktionsgeschwindigkeit, indem Sie so schnell wie möglich auf den Farbwechsel des Bildschirms reagieren.");
+            ImGui::Text("SPIELREGELN: Spielstart:\n"
+                        "\n"
+                        "Starten Sie das Spiel, indem Sie auf die Schaltfläche \"Start\" klicken.\n"
+                        "Warten:\n"
+                        "\n"
+                        "Nachdem Sie das Spiel gestartet haben, wird der Bildschirm in einer neutralen Farbe (z.B. Weiß) angezeigt.\n"
+                        "Warten Sie geduldig, bis der Bildschirm seine Farbe wechselt.\n"
+                        "Farbwechsel:\n"
+                        "\n"
+                        "Der Bildschirm wechselt zufällig nach einer bestimmten Zeitspanne seine Farbe (z.B. zu Grün).\n"
+                        "Reaktion:\n"
+                        "\n"
+                        "Sobald der Bildschirm die Farbe wechselt, klicken Sie so schnell wie möglich mit der Maus.\n"
+                        "Auswertung:\n"
+                        "\n"
+                        "Ihre Reaktionszeit wird in Millisekunden gemessen und angezeigt.\n"
+                        "Je schneller Ihre Reaktion, desto besser Ihr Ergebnis.");
 
             ImGui::PushStyleColor(ImGuiCol_ButtonHovered, sf::Color::Red);
             if (ImGui::Button("Zurück zum Menü")) {
