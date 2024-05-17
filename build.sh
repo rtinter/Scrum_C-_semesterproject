0d96eafb2368ec5cf861da3d113bf0f8adf63457
jannik.brozy <jannik.brozy@student.fh-kiel.de>
2024-05-17 21:16:32 +0000

    Update file build.sh

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+function info(){
+    echo $CI_PROJECT_DIR
+    echo $CI_COMMIT_TAG
+}
+
+function build() {
+    rm -rf build
+    mkdir -p build
+    cd build || exit 1
+    cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+    cmake --build . --config Release -j $(nproc)
+}
+
+function release() {
+    zip -j -r release.zip "$(pwd)/bin/Athena"
+    cp "$(pwd)/release.zip" "$CI_PROJECT_DIR/athena-release-$CI_COMMIT_TAG.zip"
+
+    echo "BUILD_JOB_ID=$CI_JOB_ID" >> "$CI_PROJECT_DIR/build.env"
+}
+
+if [ "$1" == "Release" ]; then
+    info
+    build
+    release
+fi
+
+if [ "$1" == "Build-Only" ]; then
+    info
+    build
+fi
