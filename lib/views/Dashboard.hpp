b7d38ccc37a2c1308c90375ace16ec326343629b
Rene Tinter <rene.tinter@googlemail.com>
2024-05-17 16:28:14 +0200

    Mod: Dashboard and Header implemented. Header modular Dashboard modular deleted whitespaces Buttonclick via callback
    deleted getter and setter to reduce the bloat

--- lib/views/Dashboard.hpp
+++ lib/views/Dashboard.hpp
@@ -3,12 +3,11 @@
 
 class Dashboard {
 
-
-
     std::vector<ui_elements::Tile> category1Tiles;
     std::vector<ui_elements::Tile> category2Tiles;
 
 public:
+
     void addTileToCategory1(const ui_elements::Tile& tile);
     void addTileToCategory2(const ui_elements::Tile& tile);
     void render();
