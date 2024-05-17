ec6fc7a5e781251eb766aced10d49b245f6a4f23
Noah Zepner <noah.zepner@student.fh-kiel.de>
2024-05-17 17:01:49 +0200

    refactor: use new Window class for the header and refactor usage in Dashboard for cleaner style

--- lib/views/Header.cpp
+++ lib/views/Header.cpp
@@ -1,25 +1,28 @@
 #include "Header.hpp"
+
+#include <utility>
+#include "Window.hpp"
+
 #pragma once
 
-Header::Header(const std::string& left, const std::string& right, std::function<void()> callback)
-        : leftText(left), rightButtonText(right), buttonClickCallback(callback) {
+Header::Header(std::string  left, std::string  right, std::function<void()> callback)
+        : leftText(std::move(left)), rightButtonText(std::move(right)), buttonClickCallback(std::move(callback)) {
     centerText = "Human Benchmark";
 }
 
 void Header::render() {
-    ImGui::Begin("Header", nullptr, ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
-    ImGui::SetWindowPos(ImVec2(0, 0), ImGuiCond_Always);
-    ImGui::SetWindowSize(ImVec2(ImGui::GetIO().DisplaySize.x, 50), ImGuiCond_Always);
+    ui_elements::Window("Header").render([this](){
+        ImGui::SetWindowPos(ImVec2(0, 0), ImGuiCond_Always);
+        ImGui::SetWindowSize(ImVec2(ImGui::GetIO().DisplaySize.x, 50), ImGuiCond_Always);
 
-    ImGui::Text("%s", leftText.c_str());
-    ImGui::SameLine(ImGui::GetIO().DisplaySize.x / 2 - ImGui::CalcTextSize(centerText.c_str()).x / 2);
-    ImGui::Text("%s", centerText.c_str());
-    ImGui::SameLine(ImGui::GetIO().DisplaySize.x - ImGui::CalcTextSize(rightButtonText.c_str()).x - 10);
-    if (ImGui::Button(rightButtonText.c_str())) {
-        if (buttonClickCallback) {
-            buttonClickCallback();
+        ImGui::Text("%s", leftText.c_str());
+        ImGui::SameLine(ImGui::GetIO().DisplaySize.x / 2 - ImGui::CalcTextSize(centerText.c_str()).x / 2);
+        ImGui::Text("%s", centerText.c_str());
+        ImGui::SameLine(ImGui::GetIO().DisplaySize.x - ImGui::CalcTextSize(rightButtonText.c_str()).x - 10);
+        if (ImGui::Button(rightButtonText.c_str())) {
+            if (buttonClickCallback) {
+                buttonClickCallback();
+            }
         }
-    }
-
-    ImGui::End();
+    });
 }
