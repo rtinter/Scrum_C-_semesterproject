bce65ef560f50488f9cfaa5f6a266feee29fa86b
Rene Tinter <rene.tinter@googlemail.com>
2024-05-23 17:32:50 +0200

    Merged dev into issue 13. works

--- src/App.cpp
+++ src/App.cpp
@@ -8,6 +8,9 @@
 #include "Fonts.hpp"
 #include "Colors.hpp"
 #include "ColorTheme.hpp"
+#include "Dashboard.hpp"
+#include "Header.hpp"
+#include "iostream"
 
 #include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
@@ -20,15 +23,27 @@ const std::string App::_title{"Human Benchmark"};
 const int App::_frameRate{60};
 
 void App::start() {
-    sf::RenderWindow window(sf::VideoMode(_windowWidth, _windowHeight), _title);
-    window.setFramerateLimit(_frameRate);
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Human Benchmark");
+    window.setFramerateLimit(60);
 
     [[maybe_unused]] auto _ = ImGui::SFML::Init(window);
-
-
     commons::StyleManager::loadStyle();
 
     sf::Clock deltaClock;
+
+    views::Dashboard dashboard;
+    //Testcallback funktion, da atm keine Logik
+    Header header("Home", "Meine Stats", []() {
+        std::cout << "Stats button clicked!" << std::endl;
+    });
+
+
+    // Füge Tiles zur Kategorie 1 hinzu
+    dashboard.addTileToCategory1(ui_elements::Tile("Pictogram", "Spielname", "Beschreibung"));
+    dashboard.addTileToCategory1(ui_elements::Tile("Pictogram", "Spielname", "Beschreibung"));
+
+    // Füge Tiles zur Kategorie 2 hinzu
+    dashboard.addTileToCategory2(ui_elements::Tile("Pictogram", "Spielname", "Beschreibung"));
 
     while (window.isOpen()) {
         sf::Event event;
@@ -39,59 +54,64 @@ void App::start() {
                 window.close();
             }
         }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+        window.clear();
+
+        header.render();
+        dashboard.render();
+
+
+        /* Style Example */
+        ImGui::ShowDemoWindow();
+
+
+        ImGui::SetNextWindowSize(ImVec2(1100.f, 600.f));
+        ImGui::Begin("Font & Color Example");
+
+        /* Font Example */
+        ImGui::PushFont(commons::Fonts::_header1);
+        ImGui::Text("Header 1");
+        ImGui::PopFont();
+
+        ImGui::PushFont(commons::Fonts::_header2);
+        ImGui::Text("Header 2");
+        ImGui::PopFont();
+
+        ImGui::PushFont(commons::Fonts::_header3);
+        ImGui::Text("Header 3");
+        ImGui::PopFont();
+
+        ImGui::PushFont(commons::Fonts::_body);
+        ImGui::Text("Body:\n"
+                    "Lorem ipsum dolor sit amet, consetetur sadipscing elitr,\n"
+                    "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.\n");
+        ImGui::PopFont();
+
+        /* Color Example */
+        // choose freely
+        auto myColor{commons::Colors::kINDIGO};
+
+        // or use Color from ColorTheme
+        auto successColor{commons::ColorTheme::kSUCCESS_COLOR};
+
+        // Use PushStyleColor()
+        ImGui::PushStyleColor(ImGuiCol_Text, myColor);
+        ImGui::Text("My Color");
+        ImGui::PopStyleColor();
+
+        // Or use TextColored()
+        ImGui::TextColored(successColor, "Success Color");
+        ImGui::TextColored(commons::ColorTheme::kERROR_COLOR, "Error Color");
+        ImGui::TextColored(commons::ColorTheme::kWARNING_COLOR, "Warning Color");
+        ImGui::TextColored(commons::ColorTheme::kINFO_COLOR, "Info Color");
+        ImGui::TextColored(commons::ColorTheme::kACCENT_COLOR, "Accent Color");
+
+        ImGui::End();
+
+        ImGui::SFML::Render(window);
+        window.display();
     }
 
-    ImGui::SFML::Update(window, deltaClock.restart());
-    window.clear();
-
-    /* Style Example */
-    ImGui::ShowDemoWindow();
-
-
-    ImGui::SetNextWindowSize(ImVec2(1100.f, 600.f));
-    ImGui::Begin("Font & Color Example");
-
-    /* Font Example */
-    ImGui::PushFont(commons::Fonts::_header1);
-    ImGui::Text("Header 1");
-    ImGui::PopFont();
-
-    ImGui::PushFont(commons::Fonts::_header2);
-    ImGui::Text("Header 2");
-    ImGui::PopFont();
-
-    ImGui::PushFont(commons::Fonts::_header3);
-    ImGui::Text("Header 3");
-    ImGui::PopFont();
-
-    ImGui::PushFont(commons::Fonts::_body);
-    ImGui::Text("Body:\n"
-                "Lorem ipsum dolor sit amet, consetetur sadipscing elitr,\n"
-                "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.\n");
-    ImGui::PopFont();
-
-    /* Color Example */
-    // choose freely
-    auto myColor{commons::Colors::kINDIGO};
-
-    // or use Color from ColorTheme
-    auto successColor{commons::ColorTheme::kSUCCESS_COLOR};
-
-    // Use PushStyleColor()
-    ImGui::PushStyleColor(ImGuiCol_Text, myColor);
-    ImGui::Text("My Color");
-    ImGui::PopStyleColor();
-
-    // Or use TextColored()
-    ImGui::TextColored(successColor, "Success Color");
-    ImGui::TextColored(commons::ColorTheme::kERROR_COLOR, "Error Color");
-    ImGui::TextColored(commons::ColorTheme::kWARNING_COLOR, "Warning Color");
-    ImGui::TextColored(commons::ColorTheme::kINFO_COLOR, "Info Color");
-    ImGui::TextColored(commons::ColorTheme::kACCENT_COLOR, "Accent Color");
-
-    ImGui::End();
-
-    ImGui::SFML::Render(window);
-    window.display();
     ImGui::SFML::Shutdown();
 }
