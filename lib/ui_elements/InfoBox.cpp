43efc7f9f57aae3161052388eefd53c46640b150
zarathustra <timo.michaelis@student.fh-kiel.de>
2024-05-23 18:52:57 +0200

    overlay und infobox mit lambda geadded

---
+++ lib/ui_elements/InfoBox.cpp
@@ -0,0 +1,8 @@
+//
+// Created by zarathustra on 23.05.24.
+//
+
+#include "InfoBox.h"
+
+namespace UiElement {
+} // UiElement
---
+++ lib/ui_elements/InfoBox.h
@@ -0,0 +1,16 @@
+//
+// Created by zarathustra on 23.05.24.
+//
+
+#ifndef INFOBOX_H
+#define INFOBOX_H
+
+namespace UiElement {
+
+class InfoBox {
+
+};
+
+} // UiElement
+
+#endif //INFOBOX_H
