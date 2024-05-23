f1378c581dac32599209756d49e1902c5ff89a7f
Noah Zepner <noah.zepner@student.fh-kiel.de>
2024-05-23 16:15:04 +0200

    merge: origin/dev into this branch

--- src/App.cpp
+++ src/App.cpp
@@ -4,6 +4,10 @@
 
 #include "App.hpp"
 #include "imgui-SFML.h"
+#include "StyleManager.hpp"
+#include "Fonts.hpp"
+#include "Colors.hpp"
+#include "ColorTheme.hpp"
 
 #include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
@@ -18,10 +22,11 @@ const int App::_frameRate{60};
 void App::start() {
     sf::RenderWindow window(sf::VideoMode(_windowWidth, _windowHeight), _title);
     window.setFramerateLimit(_frameRate);
+
     [[maybe_unused]] auto _ = ImGui::SFML::Init(window);
 
-
     sf::Clock deltaClock;
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -31,12 +36,59 @@ void App::start() {
                 window.close();
             }
         }
-
-        ImGui::SFML::Update(window, deltaClock.restart());
-
-
-        window.clear();
-        ImGui::SFML::Render(window);
-        window.display();
     }
+
+    ImGui::SFML::Update(window, deltaClock.restart());
+    window.clear();
+
+    /* Style Example */
+    ImGui::ShowDemoWindow();
+
+
+    ImGui::SetNextWindowSize(ImVec2(1100.f, 600.f));
+    ImGui::Begin("Font & Color Example");
+
+    /* Font Example */
+    ImGui::PushFont(commons::Fonts::_header1);
+    ImGui::Text("Header 1");
+    ImGui::PopFont();
+
+    ImGui::PushFont(commons::Fonts::_header2);
+    ImGui::Text("Header 2");
+    ImGui::PopFont();
+
+    ImGui::PushFont(commons::Fonts::_header3);
+    ImGui::Text("Header 3");
+    ImGui::PopFont();
+
+    ImGui::PushFont(commons::Fonts::_body);
+    ImGui::Text("Body:\n"
+                "Lorem ipsum dolor sit amet, consetetur sadipscing elitr,\n"
+                "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.\n");
+    ImGui::PopFont();
+
+    /* Color Example */
+    // choose freely
+    auto myColor{commons::Colors::kINDIGO};
+
+    // or use Color from ColorTheme
+    auto successColor{commons::ColorTheme::kSUCCESS_COLOR};
+
+    // Use PushStyleColor()
+    ImGui::PushStyleColor(ImGuiCol_Text, myColor);
+    ImGui::Text("My Color");
+    ImGui::PopStyleColor();
+
+    // Or use TextColored()
+    ImGui::TextColored(successColor, "Success Color");
+    ImGui::TextColored(commons::ColorTheme::kERROR_COLOR, "Error Color");
+    ImGui::TextColored(commons::ColorTheme::kWARNING_COLOR, "Warning Color");
+    ImGui::TextColored(commons::ColorTheme::kINFO_COLOR, "Info Color");
+    ImGui::TextColored(commons::ColorTheme::kACCENT_COLOR, "Accent Color");
+
+    ImGui::End();
+
+    ImGui::SFML::Render(window);
+    window.display();
+    ImGui::SFML::Shutdown();
 }
