3f1966d57a83f15beae4509c1e548b59ab40aaf7
Rene Tinter <rene.tinter@googlemail.com>
2024-05-23 13:20:51 +0200

    Fixed Colors for ARM64 Added Colors.cpp to find it via CMakeLists.txt

---
+++ lib/commons/Colors.cpp
@@ -0,0 +1,13 @@
+#include "Colors.hpp"
+
+namespace commons {
+    const ImVec4 Colors::kNONE = ImVec4(0, 0, 0, 0);
+    const ImVec4 Colors::kSEAFOAM = ImVec4(0.5f, 1.0f, 0.5f, 1.0f);
+    const ImVec4 Colors::kLIGHT_GRAY = ImVec4(0.8f, 0.8f, 0.8f, 1.0f);
+    const ImVec4 Colors::kDARK_GRAY = ImVec4(0.2f, 0.2f, 0.2f, 1.0f);
+    const ImVec4 Colors::kORANGE = ImVec4(1.0f, 0.65f, 0.0f, 1.0f);
+    const ImVec4 Colors::kINDIGO = ImVec4(0.29f, 0.0f, 0.51f, 1.0f);
+    const ImVec4 Colors::kBLUE = ImVec4(0.0f, 0.0f, 1.0f, 1.0f);
+    const ImVec4 Colors::kGREEN = ImVec4(0.0f, 1.0f, 0.0f, 1.0f);
+    const ImVec4 Colors::kRED = ImVec4(1.0f, 0.0f, 0.0f, 1.0f);
+}
--- lib/commons/Colors.hpp
+++ lib/commons/Colors.hpp
@@ -1,26 +1,17 @@
-#ifndef ATHENA_COLORS_H
-#define ATHENA_COLORS_H
-
-#include "imgui.h"
+#pragma once
+#include <imgui.h>
 
 namespace commons {
-
     class Colors {
     public:
-        Colors() = delete;
-
-        static ImVec4 constexpr kNONE{0, 0, 0, 0};
-        static ImVec4 constexpr kLIGHT_GRAY{0.92, 0.92, 0.92, 1};
-        static ImVec4 constexpr kDARK_GRAY{0.22, 0.22, 0.22, 1};
-        static ImVec4 constexpr kORANGE{0.9, 0.53, 0.1, 1};
-        static ImVec4 constexpr kBLUE{0.08, 0.45, 0.9, 1};
-        static ImVec4 constexpr kRED{0.79, 0.15, 0.18, 1};
-        static ImVec4 constexpr kGREEN{0.f, 0.63f, 0.f, 1.f};
-        static ImVec4 constexpr kINDIGO{0.27, 0.27, 0.78, 1};
-        static ImVec4 constexpr kSEAFOAM{0.15, 0.65, 0.67, 1};
+        static const ImVec4 kNONE;
+        static const ImVec4 kSEAFOAM;
+        static const ImVec4 kLIGHT_GRAY;
+        static const ImVec4 kDARK_GRAY;
+        static const ImVec4 kORANGE;
+        static const ImVec4 kINDIGO;
+        static const ImVec4 kBLUE;
+        static const ImVec4 kGREEN;
+        static const ImVec4 kRED;
     };
-
-
-} // commons
-
-#endif //ATHENA_COLORS_H
+}
