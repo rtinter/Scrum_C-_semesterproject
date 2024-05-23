6c10a5410ad50312ac3e17c92be1e38e34089176
Sebastian Elisa Reineke <elisa.reineke@student.fh-kiel.de>
2024-05-23 17:21:46 +0200

    refactor:  use Window class to create Tiles

--- lib/ui_elements/Tile.cpp
+++ lib/ui_elements/Tile.cpp
@@ -1,5 +1,6 @@
 #include "Tile.hpp"
 #include "../commons/Fonts.hpp"
+#include "Window.hpp"
 #include <sstream>
 #include <utility>
 
@@ -22,19 +23,21 @@ namespace ui_elements {
 
 // Render-Methode
     void Tile::render() const {
-        ImGui::Begin("Dashboard", nullptr,
-                     ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
-                     ImGuiWindowFlags_NoDecoration);
-        ImGui::SetWindowPos(ImVec2(0, 50), ImGuiCond_Always);
-        ImGui::SetWindowSize(ImVec2(ImGui::GetIO().DisplaySize.x, ImGui::GetIO().DisplaySize.y - 50), ImGuiCond_Always);
+        ui_elements::Window("Dashboard").render([this]() {
+            ImGui::SetWindowPos(ImVec2(0, 50), ImGuiCond_Always);
+            ImGui::SetWindowSize(ImVec2(ImGui::GetIO().DisplaySize.x, ImGui::GetIO().DisplaySize.y - 50),
+                                 ImGuiCond_Always);
 
-        ImGui::PushFont(commons::Fonts::_header2);
-        if (ImGui::Button(_buttonText.c_str(), ImVec2(this->_width, this->_height))) {
-            // Button action
-        }
-        ImGui::PopFont();
+            ImGui::PushFont(commons::Fonts::_header2);
+            if (ImGui::Button(_buttonText.c_str(), ImVec2(this->_width, this->_height))) {
+                // Button action
+            }
+            ImGui::PopFont();
 
-        ImGui::End();
+
+        });
     }
-
 }
+
+
+
