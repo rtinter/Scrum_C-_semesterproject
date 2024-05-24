85bcc2b835d82b58f53e85d1717e9776727ae7b2
Noah Zepner <noah.zepner@student.fh-kiel.de>
2024-05-24 13:06:30 +0200

    add: a wrapper to have a convenient way to center text

---
+++ lib/ui_elements/TextCentered.cpp
@@ -0,0 +1,16 @@
+//
+// Created by zepi on 24.05.24.
+//
+
+#include "TextCentered.hpp"
+#include "imgui.h"
+
+namespace ui_elements {
+
+    TextCentered::TextCentered(const char* content) {
+        ImVec2 textSize = ImGui::CalcTextSize(content);
+
+        ImGui::SetCursorPosX((ImGui::GetWindowWidth() - textSize.x) / 2.0f);
+        ImGui::Text("%s", content);
+    }
+} // ui_elements
