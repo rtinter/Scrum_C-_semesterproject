df2ccf2c64a2d56238687994e78fe02c43e2ded0
Rene Tinter <rene.tinter@googlemail.com>
2024-06-07 19:33:37 +0200

    Forced ARM64 Issue. Template in GameScene.cpp.

--- lib/scene/GameScene.cpp
+++ lib/scene/GameScene.cpp
@@ -1,1 +1,21 @@
-//empty but needed for cmake
+#include "GameScene.hpp"
+#include "SceneManager.hpp"
+#include "DashboardScene.hpp"
+
+
+template<typename T>
+GameScene<T>::GameScene() : header("Game Title", "Back", []() {
+    SceneManager::getInstance().switchTo(std::make_unique<DashboardScene>());
+}), _game{std::make_unique<T>()} {
+}
+
+template<typename T>
+void GameScene<T>::render() {
+    _game->start();   // Ensure T has a method named `start`
+    header.render();  // Assuming `Header` has a method named `render`
+}
+
+template<typename T>
+std::string GameScene<T>::getName() const {
+    return "GameScene"; // Can be made more dynamic based on T
+}
