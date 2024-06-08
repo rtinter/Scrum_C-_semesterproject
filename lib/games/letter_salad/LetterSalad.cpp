37e9f25b4a4efe7b7255238b93f593c79ce6ece0
benjamin.puhani <benjamin.puhani@student.fh-kiel.de>
2024-06-08 16:48:23 +0200

    ADD: create 2D Matrix of letter and value pairs, that can be selected

--- lib/games/letter_salad/LetterSalad.cpp
+++ lib/games/letter_salad/LetterSalad.cpp
@@ -3,6 +3,8 @@
 //
 
 #include "LetterSalad.hpp"
+#include "Window.hpp"
+#include "Centered.hpp"
 
 namespace game {
 
@@ -11,11 +13,44 @@ std::string LetterSalad::getName() const {
 }
 
 void LetterSalad::render() {
+    ui_elements::Window("LetterSalad Game").render([this]() {
+      ui_elements::Centered([this]() {
+        ImGui::PushStyleVar(ImGuiStyleVar_SelectableTextAlign,
+                            ImVec2(0.5f, 0.5f));
 
+        for (int y = 0; y < _gameField.size(); y++) {
+            auto &row = _gameField[y];
+
+            for (int x = 0; x < row.size(); x++) {
+                // Always print on the same line but the first cell
+                if (x > 0) ImGui::SameLine();
+
+                // PushID is used to ensure that each cell has a unique ID
+                ImGui::PushID(y * 20+x);
+                if (ImGui::Selectable(_gameField[y][x].first.c_str(),
+                                      _gameField[y][x].second,
+                                      0,
+                                      ImVec2(20, 20))) {
+
+                    // Toggle clicked cell
+                    _gameField[y][x].second ^= 1;
+                }
+                ImGui::PopID();
+            }
+        }
+        ImGui::PopStyleVar();
+      });
+    });
 }
+
 void LetterSalad::renderGame() {
 
 }
+
+void LetterSalad::update() {
+
+}
+
 void LetterSalad::stop() {
     Game::stop();
 }
--- lib/games/letter_salad/LetterSalad.hpp
+++ lib/games/letter_salad/LetterSalad.hpp
@@ -4,10 +4,16 @@
 
 #ifndef ATHENA_LIB_GAMES_LETTER_SALAD_LETTERSALAD_HPP_
 #define ATHENA_LIB_GAMES_LETTER_SALAD_LETTERSALAD_HPP_
+
 #include "Game.hpp"
 namespace game {
 
+using CharVector2D = std::vector<std::vector<std::pair<std::string, bool>>>;
+
 class LetterSalad : Game {
+  // initialize game field with 20x20 "x"
+  CharVector2D _gameField{20, {20, {"X", false}}};
+  void update();
  public:
   void stop() override;
   std::string getName() const override;
