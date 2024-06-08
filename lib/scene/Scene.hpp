cf8be722e61a490450fc57137bd44053b4b6af6e
benjamin.puhani <benjamin.puhani@student.fh-kiel.de>
2024-06-08 12:25:57 +0200

    FIX: made tiles unique_pointers and fix formating

--- lib/scene/Scene.hpp
+++ lib/scene/Scene.hpp
@@ -3,8 +3,7 @@
 #include <string>
 
 class Scene {
-public:
-    virtual ~Scene() = default;
-    virtual void render() = 0;
-    virtual std::string getName() const = 0;
+ public:
+  virtual void render() = 0;
+  virtual std::string getName() const = 0;
 };
