1ec8078944bc59825a3d42796d8aa16bad35b496
Caspar Quast <caspar.quast@student.fh-kiel.de>
2024-06-10 18:56:41 +0200

    Add first draft of Calc game functions

--- lib/games/calc/Calc.cpp
+++ lib/games/calc/Calc.cpp
@@ -1,4 +1,40 @@
 #include "Calc.hpp"
+#include "Overlay.hpp"
+#include <iostream>
+#include <random>
+#include <ctime>
+#include <sstream>
+#include <thread>
 
-namespace calc {
-} // calc
+namespace games {
+    Calc::Calc() {
+    }
+
+    void Calc::start() {
+    }
+
+    void Calc::reset() {
+    }
+
+    void Calc::render() {
+    }
+
+    void Calc::renderGame() {
+    }
+
+    void Calc::stop() {
+    }
+
+    void Calc::updateStatistics() {
+    }
+
+    void Calc::generateTasks() {
+    }
+
+    void Calc::displayTasks() {
+    }
+
+    int Calc::calculateResult(const std::string &task) {
+        return 0;
+    }
+}
--- lib/games/calc/Calc.hpp
+++ lib/games/calc/Calc.hpp
@@ -1,12 +1,36 @@
 #ifndef ATHENA_CALC_H
 #define ATHENA_CALC_H
 
-namespace calc {
+#include <string>
+#include <vector>
+#include "Game.hpp"
+#include "Timer.hpp"
 
-    class Calc {
+namespace games {
+    class Calc : public Game {
+    private:
+        int _currentLevel{1};
+        int _currentScore{0};
+        int _numberOfCorrectAnswers{0};
+        int _numberOfTasks{5};
+        std::vector<std::string> _currentTasks;
+        bool _isGameRunning{false};
+        ui_elements::Timer _taskTimer{"Task Timer", 30};
+        std::chrono::seconds _displayDuration{2};
 
+        void generateTasks();
+        void displayTasks();
+        int calculateResult(const std::string &task);
+
+    public:
+        Calc();
+        void render() override;
+        void renderGame() override;
+        void start() override;
+        void reset() override;
+        void stop() override;
+        void updateStatistics() override;
     };
+}
 
-} // calc
-
-#endif //ATHENA_CALC_H
+#endif // ATHENA_CALC_H
