42592cd80945e6b2496ff767f7ac4ba9bb3e003c
jane <janeluciaschoenfeld@gmail.com>
2024-06-13 15:25:03 +0200

    feat(165): add fire department and police texts

---
+++ lib/games/typeracer/fireDepartmentAndPoliceTexts.h
@@ -0,0 +1,14 @@
+
+#ifndef ATHENA_FIREDEPARTMENTANDPOLICETEXTS_H
+#define ATHENA_FIREDEPARTMENTANDPOLICETEXTS_H
+
+#include <vector>
+#include <string>
+
+namespace typeracer {
+
+    struct FireDepartmentAndPoliceTexts {
+        static std::vector<std::string> _mixedTexts;
+    };
+}
+#endif //ATHENA_FIREDEPARTMENTANDPOLICETEXTS_H
