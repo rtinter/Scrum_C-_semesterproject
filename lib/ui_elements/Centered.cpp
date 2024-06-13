bb281df1861177bc2ae85dd2c0ea05d4033fd33f
Noah Zepner <zepi@ZEPI-Notebook.localdomain>
2024-06-13 16:27:29 +0200

    refactor: center horizontally works now

--- lib/ui_elements/Centered.cpp
+++ lib/ui_elements/Centered.cpp
@@ -3,16 +3,31 @@
 #include <imgui.h>
 
 namespace ui_elements {
-    Centered::Centered(const std::function<void()> &callback) {
-        ImGui::BeginGroup();
+    Centered::Centered(const std::function<void()> &content) {
+        float availableWidth = ImGui::GetContentRegionAvail().x;
+        float initialCursorPosX = ImGui::GetCursorPos().x;
 
-        ImVec2 windowSize = ImGui::GetWindowSize();
-        ImGui::SetCursorPosX((windowSize.x - ImGui::CalcItemWidth() / 2) * 0.5f);
+        ImGui::Dummy(ImVec2(0, 0));
+        float leftDummyCursorPosX = ImGui::GetCursorPos().x;
 
-        ImGui::BeginGroup();
-        callback();
-        ImGui::EndGroup();
-        ImGui::EndGroup();
+        ImGui::PushClipRect(ImVec2(0, 0), ImVec2(0, 0), true);
+        content();
+        ImGui::PopClipRect();
+        float contentCursorPosX = ImGui::GetCursorPos().x;
+        float contentWidth = contentCursorPosX - leftDummyCursorPosX;
+
+        float padding = (availableWidth - contentWidth) / 2.0f;
+
+
+        ImGui::SetCursorPosX(initialCursorPosX);
+        ImGui::Dummy(ImVec2(padding, 200));
+        ImGui::SameLine();
+
+        content();
+        ImGui::SameLine();
+
+        ImGui::Dummy(ImVec2(padding, 200));
+
     }
 
 } // ui_elements
