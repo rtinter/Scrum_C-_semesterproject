96f870fd02f1fb9423c595e1f36d588c047c1c8d
Karlina Jung <karlina.jung@student.fh-kiel.de>
2024-06-13 17:16:04 +0200

    add: new methods and attributes

--- lib/games/sequence/Sequence.hpp
+++ lib/games/sequence/Sequence.hpp
@@ -5,20 +5,28 @@
 
 namespace sequence {
 
-    class Sequence : public Game {
+    class Sequence : public abstract_game::Game {
 
         std::string _endBoxString;
         int _longestReproducedSequence;
 
-        int const _NUMBER_OF_BUTTONS {9};
+        static int constexpr _NUMBER_OF_BUTTONS {9};
 
-        int _levelCounter {1};
+        int _levelCounter;
+
+        std::vector<int> _buttonsClickedSequence;
+        //true -> Button lights up
+        std::vector<bool> _buttonStates;
 
 
 
         void displayButtons();
 
         void isClickedInCorrectOrder();
+
+        void chooseNextRandomButton();
+
+        void showSequence();
 
     public:
         Sequence();
