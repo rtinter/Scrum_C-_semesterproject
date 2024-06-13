1d6b5ee3954c1a6b591c19db11665b9c4709de84
Rene Tinter <rene.tinter@googlemail.com>
2024-06-13 18:12:46 +0200

    ADD: Submit all button MOD: displayCenteredText for the statement text. ADD: Combobuttons

--- lib/games/remembering/Remembering.cpp
+++ lib/games/remembering/Remembering.cpp
@@ -1,5 +1,3 @@
-#pragma once
-
 #include "Remembering.hpp"
 #include <Centered.hpp>
 #include <DashboardScene.hpp>
@@ -9,11 +7,12 @@
 #include <SceneManager.hpp>
 #include <TextCentered.hpp>
 #include <Window.hpp>
-
+#include <sstream>
+#include <algorithm>
 
 namespace games {
 
-    Remembering::Remembering() {
+    Remembering::Remembering() : Game(abstract_game::GameID::REMEMBERING) {
         _gameName = "Remembering";
         _gameDescription =
                 "Unser Merkspiel bewertet die Fähigkeit, sich schnell und präzise Fakten zu merken,\n"
@@ -28,10 +27,32 @@ namespace games {
                      "Beachte die Rechtschreibung und schreibe zahlen aus ";
         _gameControls = "Linke Maustaste um das Feld in das du schreiben möchtest auszuwählen\n"
                         "Tastatur um deine Antwort einzugeben.";
+
+        initializeQuestionsAndAnswers(); // Initialize the questions and answers
+    }
+
+    void Remembering::initializeQuestionsAndAnswers() {
+        questions = {
+                {"In welchem Monat fand das Ereignis statt?", {"Januar",          "Mai",             "Juni",        "Dezember"},   2},
+                {"Um wieviel Uhr fand das Ereignis statt?",   {"13:00",           "14:30",           "15:00",       "16:30"},      1},
+                {"In welchem Café saß die Person?",           {"Bäckerei",        "Kaffeekränzchen", "Restaurant",  "Bar"},        1},
+                {"In welcher Straße liegt das Café?",         {"Bahnhofstraße",   "Hauptstraße",     "Nebenstraße", "Ringstraße"}, 1},
+                {"Welches Geräusch hörte die Person?",        {"Schrei",          "Sirene",          "Glas",        "Motor"},      2},
+                {"Was trug die Person?",                      {"Kapuzenpullover", "Anzug",           "T-Shirt",     "Hemd"},       0},
+                {"Welche Farbe hatte die Jeans der Person?",  {"schwarz",         "rot",             "blau",        "weiß"},       2},
+                {"Wie groß war die Person?",                  {"1,60",            "1,70",            "1,80",        "1,90"},       2},
+                {"In welche Richtung rannte die Person?",     {"Bahnhof",         "Stadtpark",       "Marktplatz",  "Kirche"},     1},
+                {"Wie heißt der Polizist?",                   {"Schmidt",         "Müller",          "Weber",       "Fischer"},    1},
+                {"Wie viel ist das Diamantarmband wert?",     {"5.000",           "10.000",          "15.000",      "20.000"},     1},
+                {"Wie viel ist die Rolex-Uhr wert?",          {"10.000",          "15.000",          "20.000",      "25.000"},     1},
+                {"Welches Auto fuhr der Täter?",              {"BMW",             "Mercedes",        "Audi",        "Volkswagen"}, 2},
+                {"Welches Kennzeichen hatte das Auto?",       {"B-CD 1234",       "B-AB 1234",       "B-EF 5678",   "B-GH 9012"},  1},
+                {"Wartete noch jemand im Auto?",              {"ja",              "nein",            "vielleicht",  "unbekannt"},  0}
+        };
     }
 
     void Remembering::render() {
-        ui_elements::InfoBox(_showInfobox, _gameName, _gameDescription, _gameRules, _gameControls, [this] {
+        ui_elements::InfoBox(_gameID, _showInfobox, _gameName, _gameDescription, _gameRules, _gameControls, [this] {
             start();
         }).render();
 
@@ -41,7 +62,7 @@ namespace games {
             ImGui::PopFont();
             ui_elements::TextCentered(std::move(_endboxText));
 
-            ui_elements::Centered([this]() {
+            ui_elements::Centered(true, true, [this]() {
                 if (ImGui::Button("Versuch es nochmal")) {
                     start();
                 }
@@ -57,11 +78,73 @@ namespace games {
         }
     }
 
+
     void Remembering::renderGame() {
         ui_elements::Window("Remembering Game").render([this]() {
-            //Logik
+            displayCenteredText(getText().c_str());
 
+            static int selectedAnswers[15] = {-1};
+            static bool submitted = false;
+
+            for (int i = 0; i < questions.size(); ++i) {
+                const auto &q = questions[i];
+                ImGui::Text("%s", q.question.c_str()); // Display the question
+
+                // Convert std::vector<std::string> to const char* array
+                std::vector<const char *> answers_cstr;
+                for (const auto &answer: q.answers) {
+                    answers_cstr.push_back(answer.c_str());
+                }
+
+                if (submitted) {
+                    // Display the question with a color based on the correctness of the answer
+                    ImVec4 color = (selectedAnswers[i] == q.correctAnswerIndex) ? ImVec4(0, 1, 0, 1) : ImVec4(1, 0, 0,
+                                                                                                              1);
+                    ImGui::PushStyleColor(ImGuiCol_Text, color);
+                    ImGui::Combo(("##combo" + std::to_string(i)).c_str(), &selectedAnswers[i], answers_cstr.data(),
+                                 answers_cstr.size());
+                    ImGui::PopStyleColor();
+                } else {
+                    ImGui::Combo(("##combo" + std::to_string(i)).c_str(), &selectedAnswers[i], answers_cstr.data(),
+                                 answers_cstr.size());
+                }
+            }
+
+            if (!submitted && ImGui::Button("Submit All")) {
+                submitted = true;
+            }
         });
+    }
+
+    std::string Remembering::getText() const {
+        return text;
+    }
+
+    void Remembering::displayCenteredText(const char *text) const {
+        ImVec2 windowSize = ImGui::GetWindowSize();
+
+        // Split the text into lines
+        std::istringstream stream(text);
+        std::string line;
+        std::vector<std::string> lines;
+
+        while (std::getline(stream, line)) {
+            lines.push_back(line);
+        }
+
+        // Display each line centered
+        for (const auto &line: lines) {
+            ImVec2 textSize = ImGui::CalcTextSize(line.c_str(), nullptr, false, windowSize.x);
+            float offsetX = (windowSize.x - textSize.x) * 0.5f;
+
+            if (offsetX > 0) {
+                ImGui::SetCursorPosX(offsetX);
+            } else {
+                ImGui::SetCursorPosX(0);
+            }
+
+            ImGui::TextWrapped("%s", line.c_str());
+        }
     }
 
     void Remembering::start() {
@@ -78,9 +161,7 @@ namespace games {
         std::fill(_userTextBuffer.begin(), _userTextBuffer.end(), 0);
     }
 
-
     void Remembering::updateStatistics() {
         // add code here
     }
-
 }
--- lib/games/remembering/Remembering.hpp
+++ lib/games/remembering/Remembering.hpp
@@ -3,6 +3,7 @@
 #include <Colors.hpp>
 #include <Game.hpp>
 #include <imgui.h>
+#include <map>
 
 #include "SFML/System/Clock.hpp"
 
@@ -11,22 +12,37 @@ namespace games {
     KOMMENTAR FÜR DAS SPIEL
     **************************************************+***/
 
-    class Remembering : public Game {
+    class Remembering : public abstract_game::Game {
         ImVec4 _windowColor{commons::Colors::LIGHT_GRAY};
 
-        //float _redDuration{0};
         std::string _userTextBuffer;
         sf::Clock _colorClock;
-        //std::chrono::steady_clock::time_point _startPoint, _finishPoint;
 
-        // static std::string getDurationRating(int duration);
+        std::string _endBoxTitleString;
+        std::string _endBoxTextString;
 
+        std::string text = "Am 12. Juni 2024, um etwa 14:30 Uhr, saß ich im Café \"Kaffeekränzchen\" in der Hauptstraße \n"
+                           "23. Plötzlich hörte ich ein lautes Geräusch von zerbrechendem Glas. Eine Person in einem \n"
+                           "schwarzen Kapuzenpullover und blauen Jeans sprang aus dem Fenster des Juweliergeschäfts \"Goldglanz\" \n"
+                           "gegenüber. Die Person, etwa 1,80 Meter groß und athletisch, trug eine schwarze Maske und eine \n"
+                           "große Sporttasche. Er rannte in Richtung Stadtpark.\n"
+                           "\n"
+                           "Zwei Minuten später kam die Polizei, sicherte den Bereich und sammelte Zeugenaussagen.\n"
+                           "Ein Polizist namens Müller nahm meine Beobachtungen auf. Die Eigentümerin des Juweliergeschäfts berichtete, dass mehrere teure Schmuckstücke,\n"
+                           "darunter ein Diamantarmband im Wert von 10.000 Euro und eine Rolex-Uhr im Wert von 15.000 Euro, gestohlen wurden.\n"
+                           "Ein weiterer Zeuge, Herr Klaus Becker, sah den Täter in ein schwarzes Audi A4 mit dem Kennzeichen B-AB 1234 einsteigen.\n"
+                           "Eine zweite Person wartete im Auto.\n";
 
-        static std::string _endBoxTitleString;
-        static std::string _endBoxTextString;
+        struct Question {
+            std::string question;
+            std::vector<std::string> answers;
+            int correctAnswerIndex;
+        };
 
+        std::vector<Question> questions;
 
     public:
+
         explicit Remembering();
 
         void render() override;
@@ -40,5 +56,12 @@ namespace games {
         void reset() override;
 
         void updateStatistics() override;
+
+        std::string getText() const;
+
+        void displayCenteredText(const char *text) const;
+
+        void initializeQuestionsAndAnswers();
+
     };
 }
