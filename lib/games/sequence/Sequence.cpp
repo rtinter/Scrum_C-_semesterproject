c4ab265b9602baa94857ff4cbc24c495101f39f8
Karlina Jung <karlina.jung@student.fh-kiel.de>
2024-06-13 17:22:58 +0200

    add: implementation of methods

--- lib/games/sequence/Sequence.cpp
+++ lib/games/sequence/Sequence.cpp
@@ -9,10 +9,12 @@
 #include "Window.hpp"
 #include "Centered.hpp"
 #include <iostream>
+#include <random>
+#include <thread>
 
 namespace sequence {
 
-    Sequence::Sequence() {
+    Sequence::Sequence() : Game(abstract_game::GameID::SEQUENCE) {
         _gameName = "Abfolge-Merken-Spiel";
         _gameDescription = "Unser Abfolge-Merken-Spiel soll die Fähigkeit testen, sich Abfolgen einzuprägen und korrekt wiederzugeben.\n";
         _gameRules = "Auf dem Bildschirm werden 9 verschiedene Buttons angezeigt.\n"
@@ -21,10 +23,12 @@ namespace sequence {
                      "Die Abfolge wird schrittweise ausgegeben. Also erst nur ein Button, dann zwei, dann drei usw.\n"
                      "bis zu maximal 20 Buttons in einer Abfolge.\n";
         _gameControls = "Linke Maustaste: Klicken der Buttons in der korrekten Reihenfolge.\n";
+
+        _buttonStates.resize(_NUMBER_OF_BUTTONS, false);
     }
 
     void Sequence::render() {
-        ui_elements::InfoBox(_showInfobox, _gameName, _gameDescription, _gameRules, _gameControls, [this] {
+        ui_elements::InfoBox(_gameID, _showInfobox, _gameName, _gameDescription, _gameRules, _gameControls, [this] {
             start();
         }).render();
 
@@ -58,7 +62,6 @@ namespace sequence {
                 ImGui::Text("Wiederhole die Reihenfolge!");
                 ImGui::NewLine();
 
-                //TODO Render buttons here
                 displayButtons();
             });
         }));
@@ -69,10 +72,15 @@ namespace sequence {
         reset();
         _isGameRunning = true;
         _showEndbox = false;
+        chooseNextRandomButton();
+
+        showSequence();
     }
 
     void Sequence::reset() {
-
+        _levelCounter = 1;
+        _buttonsClickedSequence.clear();
+        std::fill(_buttonStates.begin(), _buttonStates.end(), false); //Set all button states to false (not lighting up)
     }
 
     void Sequence::stop() {
@@ -90,25 +98,54 @@ namespace sequence {
 
     void Sequence::displayButtons() {
         ImGui::NewLine();
-        for (int i{0}; i < _NUMBER_OF_BUTTONS; i++) {
-            if (i % 3) {
+        for (int i{1}; i <= _NUMBER_OF_BUTTONS; i++) {
+            if ((i - 1) % 3) {
                 ImGui::SameLine();
             } else {
                 ImGui::NewLine();
             }
-            ImGui::PushStyleColor(ImGuiCol_Button, commons::Colors::SEAFOAM);
-            ImGui::PushStyleColor(ImGuiCol_ButtonActive, commons::ColorTheme::ACCENT_COLOR);
+            if (_buttonStates[i]) {
+                std::cout << "Light up this shit " << i << std::endl;
+                ImGui::PushStyleColor(ImGuiCol_Button, commons::ColorTheme::ACCENT_COLOR);
+            } else {
+                std::cout << "No light up :(" << i << std::endl;
+                ImGui::PushStyleColor(ImGuiCol_Button, commons::Colors::SEAFOAM);
+            }
 
-            if(ImGui::Button(std::to_string(i).c_str(), ImVec2(200, 200))) {
+            if (ImGui::Button(std::to_string(i).c_str(), ImVec2(200, 200))) {
                 std::cout << "Clicked Button " << i << std::endl;
                 isClickedInCorrectOrder();
+                start();
             }
-            ImGui::PopStyleColor(2);
+            ImGui::PopStyleColor();
         }
 
     }
 
     void Sequence::isClickedInCorrectOrder() {
 
+
+    }
+
+    /**
+     * Generates a random number in the range of 1 to 9 to choose a next button to light up for the random sequence.
+     */
+    void Sequence::chooseNextRandomButton() {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> distribution(1, _NUMBER_OF_BUTTONS);
+
+        _buttonsClickedSequence.emplace_back(distribution(gen));
+    }
+
+    void Sequence::showSequence() {
+        for (int button: _buttonsClickedSequence) {
+            _buttonStates[button - 1] = true;
+            std::cout << "Light up!" << button - 1 << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            _buttonStates[button - 1] = false;
+            std::cout << "Wait next light up!" << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        }
     }
 } // sequence
