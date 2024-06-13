c6e8ecd068abd98124f809b52c11aa9be1865fe9
noah.zepner <noah.zepner@student.fh-kiel.de>
2024-06-13 16:06:13 +0000

    Revert "Merge branch 'feature__176-center' into 'dev'"
    This reverts merge request !85

--- lib/ui_elements/Centered.cpp
+++ lib/ui_elements/Centered.cpp
@@ -1,37 +1,18 @@
-#include "Centered.hpp"
+#include <Centered.hpp>
 
 #include <imgui.h>
 
-
 namespace ui_elements {
-    Centered::Centered(bool horizontal, bool vertical, const std::function<void()> &content) {
-        ImVec2 available = ImGui::GetContentRegionAvail();
-        ImVec2 inital = ImGui::GetCursorPos();
-
-        ImGui::PushClipRect(ImVec2(0, 0), ImVec2(0, 0), true);
+    Centered::Centered(const std::function<void()> &callback) {
         ImGui::BeginGroup();
-        content();
+
+        ImVec2 windowSize = ImGui::GetWindowSize();
+        ImGui::SetCursorPosX((windowSize.x - ImGui::CalcItemWidth() / 2) * 0.5f);
+
+        ImGui::BeginGroup();
+        callback();
         ImGui::EndGroup();
-        ImVec2 contentSize = ImGui::GetItemRectSize();
-        ImGui::PopClipRect();
-
-
-        ImGui::SetCursorPos(inital);
-        ImGui::Dummy(contentSize);
-
-
-        float hPAdding = (available.x - contentSize.x) / 2.0f;
-        float vPAdding = (available.y - contentSize.y) / 2.0f;
-
-
-        if (horizontal) {
-            ImGui::SetCursorPosX(inital.x + hPAdding);
-        }
-        if (vertical) {
-            ImGui::SetCursorPosY(inital.y + vPAdding);
-        }
-        ImGui::BeginChild("##Child", contentSize);
-        content();
-        ImGui::EndChild();
+        ImGui::EndGroup();
     }
+
 } // ui_elements
