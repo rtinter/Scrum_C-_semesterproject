efd48a45545045f81d53de91706e66bfe644bb11
Caspar Quast <caspar.quast@student.fh-kiel.de>
2024-06-14 13:30:57 +0200

    add calc gameid

--- lib/abstract_game/GameIDs.hpp
+++ lib/abstract_game/GameIDs.hpp
@@ -8,7 +8,9 @@ namespace abstract_game {
         AIM_TRAINER = 1003,
         LETTER_SALAD = 1004,
         TYPERACER = 1005,
+        SEQUENCE = 1011,
         REMEMBERING = 1006,
+        CALC = 1006,
         MATRIX = 1007,
         ROWS_OF_NUMBERS = 1008,
         SEQUENCE = 1011
