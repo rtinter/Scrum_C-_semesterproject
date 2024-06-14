3d299f00150ef5fd8e055cb3db327255d05161e5
Rene Tinter <rene.tinter@googlemail.com>
2024-06-14 16:21:55 +0200

    Added Fakten merken on dashboardscene

--- lib/scene/DashboardScene.cpp
+++ lib/scene/DashboardScene.cpp
@@ -25,6 +25,7 @@ scene::DashboardScene::DashboardScene() {
     std::vector<UniqueTile> reactionCategory = {};
     std::vector<UniqueTile> accuracyCategory = {};
     std::vector<UniqueTile> problemSolvingCategory = {};
+    std::vector<UniqueTile> rememberCategory = {};
 
     // Kategorie: Reaktion
     reactionCategory.push_back(
@@ -84,10 +85,25 @@ scene::DashboardScene::DashboardScene() {
             )
     );
 
+    // Kategorie: Merkfähigkeit
+    rememberCategory.push_back(
+            std::make_unique<Tile>(
+                    "",
+                    "Fakten merken",
+                    "Merke dir so möglichst\nviele Fakten",
+                    []() {
+                        SceneManager::getInstance().switchTo(
+                                std::make_unique<GameScene<games::Remembering>>()
+                        );
+                    }
+            )
+    );
+
     /* Durch die unordered_map und die fehlende rbegin()-Funktion wird die Reihenfolge der Kategorien
      * hier festgelegt.
     */
     _dashboard->addTilesToCategory("Problemlösung", problemSolvingCategory);
+    _dashboard->addTilesToCategory("Merkfähigkeit", rememberCategory);
     _dashboard->addTilesToCategory("Genauigkeit", accuracyCategory);
     _dashboard->addTilesToCategory("Reaktion", reactionCategory);
 }
