e6262c19b0cbce4ee0b0199e3b256d6cb920966f
jane <janeluciaschoenfeld@gmail.com>
2024-06-19 22:10:23 +0200

    feat(153): add info to tables

--- lib/scene/ResultsScene.cpp
+++ lib/scene/ResultsScene.cpp
@@ -1,4 +1,3 @@
-
 #include "ResultsScene.h"
 #include "DashboardScene.hpp"
 #include "SceneManager.hpp"
@@ -8,22 +7,48 @@ namespace scene {
     ResultsScene::ResultsScene() : _header("Meine Werte", "Home", [](){
         SceneManager::getInstance().switchTo(std::make_unique<DashboardScene>());
     }){
-        //TODO Testdaten -> Hier Spieldaten einbinden!
-        std::map<int, std::vector<std::string>> map;
-        std::vector<std::string> stringvectorHeaderline {"Test", "Daten", "Für", "Tabelle"};
-        std::vector<std::string> stringvectordata {"Heute", "Tom", "300 pkt", "win"};
-        map[0] = stringvectorHeaderline;
-        map[1] = stringvectordata;
-        ui_elements::StatisticsGameTable coolTable{map};
-        _results.addGameTableContainer(ui_elements::TableContainer("hallo", coolTable));
-        _results.addGameTableContainer(ui_elements::TableContainer("hallo1", coolTable));
-        _results.addGameTableContainer(ui_elements::TableContainer("hallo3", coolTable));
-        _results.addGameTableContainer(ui_elements::TableContainer("hallo4", coolTable));
-        _results.addGameTableContainer(ui_elements::TableContainer("hallo5", coolTable));
-        _results.addGameTableContainer(ui_elements::TableContainer("hallo6", coolTable));
+        std::vector<std::string> stringvectorHeaderline {"Datum", "Länge", "Sessions", "Werte"};
 
+        std::map<int, std::vector<std::string>> dataMap;
+        std::vector<std::string> dataVector;
+        ui_elements::StatisticsGameTable dataTable{dataMap};
+
+        dataVector = {"Heute", "Tom", "300 pkt", "win"};
+        dataMap[0] = stringvectorHeaderline;
+        dataMap[1] = dataVector;
+        dataTable = ui_elements::StatisticsGameTable{dataMap};
+        _results.addGameTableContainer(ui_elements::TableContainer("Reaktion", dataTable));
+
+        dataVector = {"Heute", "Tom", "300 pkt", "win"};
+        dataMap[0] = stringvectorHeaderline;
+        dataMap[1] = dataVector;
+        dataTable = ui_elements::StatisticsGameTable{dataMap};
+        _results.addGameTableContainer(ui_elements::TableContainer("Farbe & Text", dataTable));
+
+        dataVector = {"Heute", "Tom", "300 pkt", "win"};
+        dataMap[0] = stringvectorHeaderline;
+        dataMap[1] = dataVector;
+        dataTable = ui_elements::StatisticsGameTable{dataMap};
+        _results.addGameTableContainer(ui_elements::TableContainer("Aim Trainer", dataTable));
+
+        dataVector = {"", "Tom", "300 pkt", "win"};
+        dataMap[0] = stringvectorHeaderline;
+        dataMap[1] = dataVector;
+        dataTable = ui_elements::StatisticsGameTable{dataMap};
+        _results.addGameTableContainer(ui_elements::TableContainer("Type Racer", dataTable));
+
+        dataVector = {"Heute", "Tom", "300 pkt", "win"};
+        dataMap[0] = stringvectorHeaderline;
+        dataMap[1] = dataVector;
+        dataTable = ui_elements::StatisticsGameTable{dataMap};
+        _results.addGameTableContainer(ui_elements::TableContainer("Sequence", dataTable));
+
+        dataVector = {"Heute", "Tom", "300 pkt", "win"};
+        dataMap[0] = stringvectorHeaderline;
+        dataMap[1] = dataVector;
+        dataTable = ui_elements::StatisticsGameTable{dataMap};
+        _results.addGameTableContainer(ui_elements::TableContainer("Fakten merken", dataTable));
     }
-
 
     void ResultsScene::render() {
         _header.render();
