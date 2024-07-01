7c9980b81518555bfd660237aecf647eefd650d0
Noah Zepner <zepi@ZEPI-Notebook.localdomain>
2024-07-01 12:05:01 +0200

    clean: aim_trainer

--- lib/games/aim_trainer/AimTrainer.hpp
+++ lib/games/aim_trainer/AimTrainer.hpp
@@ -1,67 +1,63 @@
 #pragma once
 
-#include "Blob.hpp"
-#include "Centered.hpp"
-#include "Fonts.hpp"
-#include "Game.hpp"
-#include "InfoBox.hpp"
-#include "Overlay.hpp"
-#include "SceneManager.hpp"
-#include "TextCentered.hpp"
 #include <future>
-#include <iostream>
-#include <thread>
+#include <SFML/System/Clock.hpp>
 
-#include "DashboardScene.hpp"
+#include "Blob.hpp"
+#include "Game.hpp"
+#include "SceneManager.hpp"
 #include "Timer.hpp"
-#include "SFML/System/Clock.hpp"
 
 namespace games {
-
-/**
- * @brief The AimTrainer class implements an aim training game.
- *
- * The game spawns blobs (circles) at random positions on the screen, and the player needs to click on them to score points.
- * The game tracks the number of successful hits and missed blobs. The game becomes progressively more challenging as the
- * number of blobs increases and their size decreases over time. A timer limits the game duration.
- */
-
-class AimTrainer : abstract_game::Game {
-private:
-    std::future<void> _backgroundBlobTask;
-    std::future<void> _spawnBlobTask;
-
-    std::vector<aim_trainer::Blob> _currentBlobs;
-
-    int _successCounter {0};
-    int _missedCounter {0};
-
-    float _spawnAmount{1};
-
-    sf::Clock _clock;
-    sf::Time _elapsedTime;
-
-    std::unique_ptr<ui_elements::Timer> _timer;
-    float _missedFactor {1};
-
     /**
-     * @brief Spawns new blobs at random positions within the window.
+     * @brief The AimTrainer class implements an aim training game.
+     *
+     * The game spawns blobs (circles) at random positions on the screen, and the player needs to click on them to score points.
+     * The game tracks the number of successful hits and missed blobs. The game becomes progressively more challenging as the
+     * number of blobs increases and their size decreases over time. A timer limits the game duration.
      */
-    void spawnBlobs();
+    class AimTrainer final : abstract_game::Game {
+        std::future<void> _backgroundBlobTask;
+        std::future<void> _spawnBlobTask;
 
-    /**
-     * @brief Updates the state of current blobs and handles missed blobs.
-     */
-    void updateBlobs();
-public:
-    AimTrainer();
+        std::vector<aim_trainer::Blob> _currentBlobs;
 
-    std::string getName() const override;
-    void render() override;
-    void renderGame() override;
-    void reset() override;
-    void start() override;
-    void stop() override;
-    void updateStatistics() override;
-};
-}
+        int _successCounter{0};
+        int _missedCounter{0};
+
+        float _spawnAmount{1};
+
+        sf::Clock _clock;
+        sf::Time _elapsedTime;
+
+        std::unique_ptr<ui_elements::Timer> _timer;
+        float _missedFactor{1};
+
+        /**
+         * @brief Spawns new blobs at random positions within the window.
+         */
+        void spawnBlobs();
+
+        /**
+         * @brief Updates the state of current blobs and handles missed blobs.
+         */
+        void updateBlobs();
+
+    public:
+        AimTrainer();
+
+        std::string getName() const override;
+
+        void render() override;
+
+        void renderGame() override;
+
+        void reset() override;
+
+        void start() override;
+
+        void stop() override;
+
+        void updateStatistics() override;
+    };
+} // games
