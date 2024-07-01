275df42f76edc79b2cc32fb826bdf9750db3826a
Noah Zepner <zepi@ZEPI-Notebook.localdomain>
2024-07-01 11:16:05 +0200

    clean: csv

--- lib/csv/CsvParser.hpp
+++ lib/csv/CsvParser.hpp
@@ -1,26 +1,20 @@
+#pragma once
 
-#ifndef ATHENA_CSVPARSER_H
-#define ATHENA_CSVPARSER_H
-
-#include <vector>
 #include <string>
-#include <fstream>
-#include <sstream>
+#include <vector>
 
 namespace csv {
-
     /*****
      * This class is responsible for parsing a CSV file.
      * It reads the file line by line and splits each line into a vector of strings.
      */
     class CsvParser {
         std::string _filename;
+
     public:
-        explicit CsvParser(std::string const &filename) : _filename(filename) {}
+        explicit CsvParser(std::string const &filename) : _filename(filename) {
+        }
 
-        std::vector<std::vector<std::string>> parse();
+        std::vector<std::vector<std::string> > parse() const;
     };
-
 } // csv
-
-#endif //ATHENA_CSVPARSER_H
