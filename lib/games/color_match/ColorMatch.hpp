526dc8338ae41babb3cff8478684829f44be38c1
Noah Zepner <zepi@ZEPI-Notebook.localdomain>
2024-07-01 12:20:59 +0200

    clean: color_match

--- lib/games/color_match/ColorMatch.hpp
+++ lib/games/color_match/ColorMatch.hpp
@@ -1,39 +1,44 @@
 #pragma once
 
 #include <array>
-#include <Colors.hpp>
-#include <Game.hpp>
-#include <string>
 #include <random>
-#include <Timer.hpp>
+#include <string>
+
+#include "Colors.hpp"
+#include "Game.hpp"
+#include "Timer.hpp"
 
 namespace games {
     /**
      * @brief: provides logic and visuals for the game 'Farbe & Wort'
      */
-    class ColorMatch : public abstract_game::Game {
-        static int const NUMBER_OF_RANDOM_COLORS{3};
+    class ColorMatch final : public abstract_game::Game {
+        static constexpr int NUMBER_OF_RANDOM_COLORS{3};
         bool _isTimeForNewRandomColors;
         int _indexOfCurrentColor;
         int _numberOfCorrectClicksInTotal;
         int _numberOfCorrectClicksSinceLastError;
         int _longestStreak;
+
         enum GameMode {
             MATCH_IMVEC4, // click text button according to font color
-            MATCH_STRING  // click color button according to word
+            MATCH_STRING // click color button according to word
         };
+
         GameMode _currentGameMode;
         ui_elements::Timer _timer{"Color Match Game", 60};
 
         std::vector<std::string> const _availableColorsString{"rot", "blau", "gelb", "lila", "grün"};
-        std::vector<ImVec4> const _availableColorsImVec4{commons::Colors::RED, commons::Colors::BLUE,
-                                                         commons::Colors::YELLOW, commons::Colors::PURPLE,
-                                                         commons::Colors::GREEN};
+        std::vector<ImVec4> const _availableColorsImVec4{
+            commons::Colors::RED, commons::Colors::BLUE,
+            commons::Colors::YELLOW, commons::Colors::PURPLE,
+            commons::Colors::GREEN
+        };
         std::vector<std::string> _randomColorsString;
         std::vector<ImVec4> _randomColorsImVec4;
         std::array<std::string, NUMBER_OF_RANDOM_COLORS> const _clickedColors;
 
-        void displayRandomColors();
+        void displayRandomColors() const;
 
         void pickRandomColorsString();
 
@@ -57,7 +62,5 @@ namespace games {
         void updateStatistics() override;
 
         void stop() override;
-
     };
-
-}
+} // games
